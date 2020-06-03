4b298d0bdd55053d807d5a10759448a43ff9604a
David Herzberg <dherzberg@wpspublish.com>
2020-06-02 22:29:16 -0700

    continue markdown of weighting process

--- CODE/weighting-data-demo.R
+++ CODE/weighting-data-demo.R
@@ -27,15 +27,6 @@ cat_order <- c(
   # Region
   NA, "northeast", "south", "midwest", "west")
 
-# create census percentages
-# gender_census <- tibble(gender = c("female", "male"),
-#                         Freq = nrow(unweighted_input)*c(0.53, 0.47))
-# educ_census <- tibble(educ = c("no_HS", "HS_grad", "some_college", "BA_plus"),
-#                       Freq = nrow(unweighted_input)*c(0.119, 0.263, 0.306, 0.311))
-# ethnic_census <- tibble(ethnic = c("hispanic", "asian", "black", "white", "other"),
-#                         Freq = nrow(unweighted_input)*c(0.239, 0.048, 0.136, 0.521, .056))
-# region_census <- tibble(region = c("northeast", "south", "midwest", "west"),
-#                         Freq = nrow(unweighted_input)*c(0.166, 0.383, 0.212, 0.238))
 gender_census <- tibble(cat = c("female", "male"),
                         Freq = nrow(unweighted_input)*c(0.53, 0.47))
 educ_census <- tibble(cat = c("no_HS", "HS_grad", "some_college", "BA_plus"),
@@ -44,6 +35,7 @@ ethnic_census <- tibble(cat = c("hispanic", "asian", "black", "white", "other"),
                         Freq = nrow(unweighted_input)*c(0.239, 0.048, 0.136, 0.521, .056))
 region_census <- tibble(cat = c("northeast", "south", "midwest", "west"),
                         Freq = nrow(unweighted_input)*c(0.166, 0.383, 0.212, 0.238))
+
 # get demo counts of unweighted input
 freq_demos_unweighted <- unweighted_input %>%
   pivot_longer(age_range:clin_status, names_to = 'var', values_to = 'cat') %>%
@@ -52,7 +44,7 @@ freq_demos_unweighted <- unweighted_input %>%
   arrange(match(var, var_order), match(cat, cat_order)) %>%
   ungroup() %>%
   mutate(
-   pct_samp = round(((n / nrow(unweighted_input)) * 100), 1)
+    pct_samp = round(((n / nrow(unweighted_input)) * 100), 1)
   ) %>%
   select(var, cat, n, pct_samp) %>% 
   full_join(region_census, by = "cat")
@@ -68,37 +60,41 @@ freq_demos_comp <- list_demos %>%
   mutate_at(vars(census_count), ~as.integer(str_replace_all(., "NA", ""))) %>% 
   mutate(census_pct = 100 * round(census_count/nrow(unweighted_input), 3),
          diff_pct = census_pct - pct_samp
-         ) %>% 
+  ) %>% 
   rename(input_count = n, input_pct = pct_samp) %>% 
   select(var, cat, input_count, census_count, input_pct, census_pct, diff_pct) %>% 
   arrange(desc(diff_pct))
 
+rm(list = setdiff(ls(), ls(pattern = "input|comp|list")))
+
+# rename vars in `_census` dfs
+list_census <- list(list_demos[2:5],
+                    c("gender", "educ", "ethnic", "region")) %>%
+  pmap(~ ..1 %>%
+         rename_at(vars(cat), ~str_replace(., "cat", !!..2))
+  )
+
+names(list_census) <- c("gender_census", "educ_census", "ethnic_census", "region_census")
+
+list2env(list_census, envir=.GlobalEnv)
+
+
 # create survey objects that represent weights
 unweighted_survey_object <- svydesign(ids = ~1, 
-                                     data = unweighted_input, 
-                                     weights = NULL)
-
-# create census percentages
-gender_census <- tibble(gender = c("female", "male"), 
-                        Freq = nrow(unweighted_input)*c(0.53, 0.47))
-educ_census <- tibble(educ = c("no_HS", "HS_grad", "some_college", "BA_plus"), 
-                      Freq = nrow(unweighted_input)*c(0.119, 0.263, 0.306, 0.311))
-ethnic_census <- tibble(ethnic = c("hispanic", "asian", "black", "white", "other"), 
-                        Freq = nrow(unweighted_input)*c(0.239, 0.048, 0.136, 0.521, .056))
-region_census <- tibble(region = c("northeast", "south", "midwest", "west"), 
-                        Freq = nrow(unweighted_input)*c(0.166, 0.383, 0.212, 0.238))
+                                      data = unweighted_input, 
+                                      weights = NULL)
 
 # rake input data to create case-wise weights
 rake_unweighted_input <- rake(design = unweighted_survey_object,
-                          sample.margins = list(~gender, ~educ, ~ethnic, ~region),
-                          population.margins = list(gender_census, educ_census, 
-                                                    ethnic_census, region_census))
+                              sample.margins = list(~gender, ~educ, ~ethnic, ~region),
+                              population.margins = list(gender_census, educ_census, 
+                                                        ethnic_census, region_census))
 
 # bind demo weights to original data
 input_demo_wts <- bind_cols(
   rake_unweighted_input[["variables"]], 
   data.frame(rake_unweighted_input[["prob"]])
-  ) %>% 
+) %>% 
   rename(demo_wt = rake_unweighted_input...prob...) %>% 
   select(ID:clin_status, demo_wt, everything()) %>% 
   arrange(desc(demo_wt))
