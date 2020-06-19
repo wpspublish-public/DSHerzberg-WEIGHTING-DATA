44305132716e804768b03c8dd7b844e10d7dde50
David Herzberg <dherzberg@wpspublish.com>
2020-06-18 23:50:14 -0700

    continue refining weighting-data demo

--- CODE/weighting-data-demo.R
+++ CODE/weighting-data-demo.R
@@ -7,7 +7,7 @@ urlRemote_path  <- "https://raw.githubusercontent.com/"
 github_path <- "DSHerzberg/WEIGHTING-DATA/master/INPUT-FILES/"
 fileName_path   <- "unweighted-input.csv"
 
-unweighted_input <- suppressMessages(read_csv(url(
+original_input <- suppressMessages(read_csv(url(
   str_c(urlRemote_path, github_path, fileName_path)
 )))
 
@@ -28,23 +28,23 @@ cat_order <- c(
   NA, "northeast", "south", "midwest", "west")
 
 gender_census <- tibble(cat = c("female", "male"),
-                        Freq = nrow(unweighted_input)*c(0.53, 0.47))
+                        Freq = nrow(original_input)*c(0.53, 0.47))
 educ_census <- tibble(cat = c("no_HS", "HS_grad", "some_college", "BA_plus"),
-                      Freq = nrow(unweighted_input)*c(0.119, 0.263, 0.306, 0.311))
+                      Freq = nrow(original_input)*c(0.119, 0.263, 0.306, 0.311))
 ethnic_census <- tibble(cat = c("hispanic", "asian", "black", "white", "other"),
-                        Freq = nrow(unweighted_input)*c(0.239, 0.048, 0.136, 0.521, .056))
+                        Freq = nrow(original_input)*c(0.239, 0.048, 0.136, 0.521, .056))
 region_census <- tibble(cat = c("northeast", "south", "midwest", "west"),
-                        Freq = nrow(unweighted_input)*c(0.166, 0.383, 0.212, 0.238))
+                        Freq = nrow(original_input)*c(0.166, 0.383, 0.212, 0.238))
 
 # get demo counts of unweighted input
-freq_demos_unweighted <- unweighted_input %>%
+freq_demos_unweighted <- original_input %>%
   pivot_longer(age_range:clin_status, names_to = 'var', values_to = 'cat') %>%
   group_by(var, cat) %>%
   count(var, cat) %>%
   arrange(match(var, var_order), match(cat, cat_order)) %>%
   ungroup() %>%
   mutate(
-    pct_samp = round(((n / nrow(unweighted_input)) * 100), 1)
+    pct_samp = round(((n / nrow(original_input)) * 100), 1)
   ) %>%
   select(var, cat, n, pct_samp) %>% 
   full_join(region_census, by = "cat")
@@ -58,7 +58,7 @@ freq_demos_comp <- list_demos %>%
   filter(!(var %in% c("age_range", "clin_status"))) %>% 
   unite(census_count, c(Freq.x, Freq.y, Freq.x.x, Freq.y.y), sep = "", remove = T) %>% 
   mutate_at(vars(census_count), ~as.integer(str_replace_all(., "NA", ""))) %>% 
-  mutate(census_pct = 100 * round(census_count/nrow(unweighted_input), 3),
+  mutate(census_pct = 100 * round(census_count/nrow(original_input), 3),
          diff_pct = census_pct - pct_samp
   ) %>% 
   rename(input_count = n, input_pct = pct_samp) %>% 
@@ -81,23 +81,25 @@ list2env(list_census, envir=.GlobalEnv)
 
 # create survey objects that represent weights
 unweighted_survey_object <- svydesign(ids = ~1, 
-                                      data = unweighted_input, 
+                                      data = original_input, 
                                       weights = NULL)
 
 # rake input data to create case-wise weights
-rake_unweighted_input <- rake(design = unweighted_survey_object,
+rake_original_input <- rake(design = unweighted_survey_object,
                               sample.margins = list(~gender, ~educ, ~ethnic, ~region),
                               population.margins = list(gender_census, educ_census, 
                                                         ethnic_census, region_census))
 
 # bind demo weights to original data
 input_demo_wts <- bind_cols(
-  rake_unweighted_input[["variables"]], 
-  data.frame(rake_unweighted_input[["prob"]])
+  rake_original_input[["variables"]],  
+  data.frame(rake_original_input[["prob"]]), 
+  data.frame(demo_wt= weights(rake_original_input))
 ) %>% 
-  rename(demo_wt = rake_unweighted_input...prob...) %>% 
-  select(ID:clin_status, demo_wt, everything()) %>% 
-  arrange(desc(demo_wt))
+  rename(samp_prob  = rake_original_input...prob...) %>% 
+  mutate(ratio = samp_prob / demo_wt) %>% 
+  select(ID:clin_status, samp_prob, demo_wt, ratio, everything()) %>% 
+  arrange(desc(samp_prob))
 
 # reality check, what demo cell is likely to have the weight that deviates farthest from 1?
 #  female, no_HS, hispanic, northeast
@@ -122,3 +124,67 @@ case_1_wt <- input_demo_wts %>%
   ) %>% 
   sample_n(1)
 
+tail(input_demo_wts)
+
+filter(input_demo_wts, between(samp_prob, .98, 1.02))
+
+head(input_demo_wts)
+
+ggplot(input_demo_wts, aes(demo_wt, samp_prob)) +
+  geom_line(color = "darkblue", size = 1) +
+  geom_point(x=1, y=1, color='purple', size = 3) + 
+  scale_x_continuous(breaks = seq(0, 6, .5), minor_breaks = seq(0, 6, .1)) +
+  scale_y_continuous(breaks = seq(0, 2.5, .5),
+                     minor_breaks = seq(0, 2.5, .1)) +
+  xlab("demographic weighting multiplier") +
+  ylab("sampling probability") +
+  annotate(
+    "text",
+    x = 1.5,
+    y = 1.5,
+    label = "Oversampled relative to census: sampling probability > 1",
+    color = "red",
+    hjust = 0
+  ) +
+  annotate(
+    "text",
+    x = 1.5,
+    y = 1.4,
+    label = "Undersampled relative to census: sampling probability < 1",
+    color = "darkgreen",
+    hjust = 0
+  ) +
+annotate(
+  "text",
+  x = 1.1,
+  y = 1.1,
+  label = "samp prob = weight = 1: demo cell pct matches census pct",
+  color = "purple",
+  hjust = 0
+)
+
+# apply weights and calculate raw scores
+
+unweighted_input <- input_demo_wts %>% 
+  select(-c(samp_prob, ratio)) %>%
+  rename_with(~ str_c("i", str_pad(
+    as.character(1:50), 2, side = "left", pad = "0"), "_uw"), 
+    i01:i50) %>% 
+  mutate(
+    TOT_raw_unweight = rowSums(.[str_c("i", str_pad(
+      as.character(1:50), 2, side = "left", pad = "0"), "_uw")])
+  )
+
+
+ID_weights <- unweighted_input %>% 
+  select(ID, demo_wt)
+    
+weighted_input <- original_input %>%
+  left_join(ID_weights, by = "ID") %>%
+  relocate(demo_wt, .before = i01) %>%
+  rename_with(~ str_c("i", str_pad(
+    as.character(1:50), 2, side = "left", pad = "0"
+  ), "_w"),
+  i01:i50) %>%
+  mutate(TOT_raw_weight = rowSums(.[str_c("i", str_pad(
+    as.character(1:50), 2, side = "left", pad = "0"), "_w")]) * demo_wt)
