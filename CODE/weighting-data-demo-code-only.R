a0b84f338846b5e6ce364efce3bc7d44819b33ce
David Herzberg <dherzberg@wpspublish.com>
2021-08-09 17:14:00 -0700

    update documentation to include lookup table for values of demo_wt associated with demographic crossings

--- CODE/weighting-data-demo-code-only.R
+++ CODE/weighting-data-demo-code-only.R
@@ -116,6 +116,30 @@ write_csv(
   na = ""
 )
 
+demo_weight_by_crossing <- weighted_output %>%
+  group_by(demo_wt) %>%
+  summarize(
+    gender = first(gender),
+    educ = first(educ),
+    ethnic = first(ethnic),
+    region = first(region)
+  ) %>%
+  relocate(demo_wt, .after = region) %>%
+  arrange(
+    match(gender, cat_order),
+    match(educ, cat_order),
+    match(ethnic, cat_order),
+    match(region, cat_order)
+  )
+
+write_csv(
+  demo_weight_by_crossing,
+  here(
+    "OUTPUT-FILES/weights-per-demo-crossing.csv" 
+  ),
+  na = ""
+)
+
 # PROOF OF CONCEPT
 
 cat_count_comp <-
